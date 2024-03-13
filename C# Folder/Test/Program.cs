c02d4d7cb81ef8535ebf34cdbf6976cb5cea95ef
hvtraanf <33631052+hvtraanf@users.noreply.github.com>
2024-03-13 16:48:31 +0700

    Update Program.cs

--- C# Folder/Test/Program.cs
+++ C# Folder/Test/Program.cs
@@ -8,7 +8,9 @@ internal class Program
         int age = 20;
 
         TestClass c = new TestClass("John", age);
+        TestClass tc = new TestClass("Test", age + 5);
 
         Console.WriteLine(c);
+        Console.WriteLine(tc);
     }
 }
